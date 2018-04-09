e8564e9666241627ebf7c5243df6c938ea83a312
mahmudamen <mahmudamen@gmail.com>
2018-04-09 11:08:45 +0200

    lession-1

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,8 +1,16 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+const searchedForText = 'hippos';
+const unsplashRequest = new XMLHttpRequest();
+
+unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+unsplashRequest.onload = addImage;
+unsplashRequest.setRequestHeader('Authorization', 'Client-ID c5c4badd34e39bef9f3f7f429abfcbae692f778d4b02f17aa222145e3ca23b3a');
+unsplashRequest.send();
+
+function addImage(){
+}
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
